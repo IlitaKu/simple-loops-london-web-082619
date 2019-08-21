9cf95428665f3470f662aa38a680d748d4b67686
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-21 09:54:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,9 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  loop do number_of_times
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -14,14 +17,20 @@ end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times.times do
+  puts phrase = "Welcome to Flatiron School's Web Development Course!"
+end
   
 end
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  i = 0
+  while number_of_times < i do
+    puts "Welcome to Flatiron School's Web Development Course!"
+    i +=1
+  end
   
 end
 
@@ -29,7 +38,10 @@ def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+i = 0
+until i != number_of_times
+puts phrase
+end
 end
 
 def for_iterator(number_of_times)
@@ -37,6 +49,8 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for word in(1..number_of_times) do
+    puts phrase
+  end
 end
 
